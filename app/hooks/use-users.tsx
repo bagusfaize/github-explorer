6fad91c6417f064357f92324d7023436e512b988
bagus faize <bagusfaize@gmail.com>
2023-09-11 17:57:28 +0700

    add use search users hooks

--- app/hooks/use-users.tsx
+++ app/hooks/use-users.tsx
@@ -1,13 +1,21 @@
+import { useState } from "react";
 import { useQuery } from "@tanstack/react-query"
-import { getUsersTemp } from "../services/github-service"
-import { GithubUserProps } from "../types/types";
+import { searchUsers } from "../services/github-service"
+import { GithubUserProps, QueryParamsProps } from "../types/types";
 
+export const useSearchUsers = ({
+    page,
+    per_page
+}: QueryParamsProps) => {
+    const [searchQuery, setSearchQuery] = useState<string>('')
 
-export const useUsers = () => {
-    const { data, isLoading } = useQuery<GithubUserProps[], Error>(["githubUsers"], () => getUsersTemp());
+    const { data, isLoading, refetch } = useQuery<GithubUserProps[], Error>(['searchUsers'], () => searchUsers({ q:searchQuery, page, per_page }), { enabled: false });
 
-    return{
+    return {
         data,
-        isLoading
+        isLoading,
+        searchQuery,
+        setSearchQuery,
+        refetch
     }
 }
