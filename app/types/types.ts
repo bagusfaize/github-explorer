05de3505b6b4b913d5f4a5c1d5eab8d2743cae8c
bagus faize <bagusfaize@gmail.com>
2023-09-11 18:32:44 +0700

    add types

--- app/types/types.ts
+++ app/types/types.ts
@@ -4,3 +4,15 @@ export type GithubUserProps = {
     id: number;
     avatar_url: string;
 }
+
+export type QueryParamsProps = {
+    q?: string,
+    page?: number,
+    per_page?: number
+}
+
+export type InputSearchProps = {
+    searchQuery: string,
+    onInputChange: (query: string) => void, 
+    onSearch: () => void
+  }
