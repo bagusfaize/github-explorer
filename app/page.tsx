7b44753df0cd72c63cd994df72421aa5d5cd81ee
bagus faize <bagusfaize@gmail.com>
2023-09-11 18:00:47 +0700

    implement use search users hooks on homepage

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,22 @@
 "use client"
 
+import InputSearch from "./components/input-search";
 import ProfileCard from "./components/profile-card";
-import { useUsers } from "./hooks/use-users"
+import { useSearchUsers } from "./hooks/use-users"
 
 export default function Home() {
+  const {
+    refetch,
+    data = [],
+    searchQuery,
+    setSearchQuery
+  } = useSearchUsers({
+    page: 1,
+    per_page: 6
+  });
 
-  const {data = [], isLoading} = useUsers();
-
-  if (isLoading) {
-    return <div>Loading...</div>
+  const handleSearch = () => {
+    refetch()
   }
 
   return (
@@ -16,10 +24,13 @@ export default function Home() {
       <div className="flex flex-col items-center justify-center">
         <div className="my-5">
           <h1 className="text-3xl text-center">GitHub Explorer</h1>
-          <input type="text" className="border border-sky-800 px-3 py-1" />
-          <button className="bg-sky-800	text-white px-3 py-1 mx-3">Search</button>
+          <InputSearch
+            searchQuery={searchQuery}
+            onInputChange={(value) => setSearchQuery(value)}
+            onSearch={handleSearch}
+          />
         </div>
-        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-5 w-full md:w-auto">
+        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-5 w-full md:w-4/5">
           {data.map(user => (
             <ProfileCard
               key={user.id}
