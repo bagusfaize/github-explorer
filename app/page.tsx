211c641db7a72941f765f1b0b9172ee51d01927d
bagus faize <bagusfaize@gmail.com>
2023-09-11 07:16:43 +0700

    try mapping user data temp

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,31 @@
+"use client"
+
+import { useUsers } from "./hooks/use-users"
 
 export default function Home() {
+
+  const {data = [], isLoading} = useUsers();
+
+  if (isLoading) {
+    return <div>Loading...</div>
+  }
+
   return (
     <div className="min-h-screen bg-gray-50">
+      <main className="flex flex-col items-center justify-center">
+        <div>
+          <h1 className="text-3xl">GitHub Explorer</h1>
+          <input type="text" className="border border-sky-800 px-3 py-1" />
+          <button className="bg-sky-800	text-white px-3 py-1 mx-3">Search</button>
+        </div>
+        <div className="flex">
+          {data.map(user => (
+            <div className="bg-white p-5 m-3" key={user.login}>
+              {user.login}
+            </div>
+          ))}
+        </div>
+      </main>
     </div>
   )
 }
