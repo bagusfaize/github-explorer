cdcbbb9021a3b9bf9eccd2872e5f9b2a07b9d9b7
bagus faize <bagusfaize@gmail.com>
2023-09-11 18:31:49 +0700

    add bg gray to body

--- app/layout.tsx
+++ app/layout.tsx
@@ -20,7 +20,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-        <body className={poppins.className}>
+        <body className={`${poppins.className} bg-gray-100`}>
           <Provider>{children}</Provider>
         </body>
       </html>
