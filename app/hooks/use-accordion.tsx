7522593fdb1e8bec87738a51a17237fbd530267d
bagus faize <bagusfaize@gmail.com>
2023-09-12 09:12:11 +0700

    fix repos open when search new user

--- app/hooks/use-accordion.tsx
+++ app/hooks/use-accordion.tsx
@@ -1,8 +1,15 @@
-import { useState } from "react";
+import { useSearchParams } from "next/navigation";
+import { useEffect, useState } from "react";
 
 
 export const useAccordion = () => {
-    const [expanded, setExpanded] = useState<boolean | number>(false)
+    const searchParams = useSearchParams();
+    const username = searchParams.get("username") || '';
+    const [expanded, setExpanded] = useState<boolean | number>(false);
+
+    useEffect(() => {
+        setExpanded(false);
+    }, [username])
 
     return {
         expanded,
